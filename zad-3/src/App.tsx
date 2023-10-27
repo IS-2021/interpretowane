ea2418810c2dc64ad7139b91d9543bf94e82d774
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-10-27 23:28:18 +0200

    feat(zad3): Add movie list by genre

--- zad-3/src/App.tsx
+++ zad-3/src/App.tsx
@@ -1,87 +1,79 @@
 import moviesData from "./data/movies-2010s.json";
+import { MovieListByGenre } from "@/components/MovieList/MovieListByGenre";
 import { MovieTable } from "@/components/MovieTable/MovieTable";
 
 function App() {
-  const movies = moviesData.slice(0, 100);
+	const movies = moviesData.slice(0, 100);
 
-  return (
-    <div className="container">
-      <h1>Baza filmów</h1>
-      <form>
-        <div className="form-group mb-2">
-          <label htmlFor="inputTitle" className="form-label">
-            Tytuł
-          </label>
-          <input
-            type="text"
-            id="inputTitle"
-            className="form-control"
-            placeholder="Podaj tytuł lub fragment tytułu filmu"
-          />
-        </div>
+	return (
+		<div className="container">
+			<h1>Baza filmów</h1>
+			<form>
+				<div className="form-group mb-2">
+					<label htmlFor="inputTitle" className="form-label">
+						Tytuł
+					</label>
+					<input
+						type="text"
+						id="inputTitle"
+						className="form-control"
+						placeholder="Podaj tytuł lub fragment tytułu filmu"
+					/>
+				</div>
 
-        <div className="form-group row mb-2">
-          <label
-            className="col-sm-4 col-form-label"
-            htmlFor="inputProductionFrom"
-          >
-            Rok produkcji od:
-          </label>
-          <div className="col-sm-8">
-            <input
-              type="text"
-              id="inputProductionFrom"
-              className="form-control"
-              placeholder="Liczba naturalna z przedziału 1900-2019"
-            />
-          </div>
-        </div>
+				<div className="form-group row mb-2">
+					<label className="col-sm-4 col-form-label" htmlFor="inputProductionFrom">
+						Rok produkcji od:
+					</label>
+					<div className="col-sm-8">
+						<input
+							type="text"
+							id="inputProductionFrom"
+							className="form-control"
+							placeholder="Liczba naturalna z przedziału 1900-2019"
+						/>
+					</div>
+				</div>
 
-        <div className="form-group row">
-          <label
-            className="col-sm-4 col-form-label"
-            htmlFor="inputProductionTo"
-          >
-            Rok produkcji do:
-          </label>
-          <div className="col-sm-8">
-            <input
-              type="text"
-              id="inputProductionTo"
-              className="form-control"
-              placeholder="Liczba naturalna z przedziału 1900-2019"
-            />
-          </div>
-        </div>
+				<div className="form-group row">
+					<label className="col-sm-4 col-form-label" htmlFor="inputProductionTo">
+						Rok produkcji do:
+					</label>
+					<div className="col-sm-8">
+						<input
+							type="text"
+							id="inputProductionTo"
+							className="form-control"
+							placeholder="Liczba naturalna z przedziału 1900-2019"
+						/>
+					</div>
+				</div>
 
-        <div className="form-group mb-2">
-          <label htmlFor="inputCast" className="form-label">
-            Obsada
-          </label>
-          <input
-            type="text"
-            id="inputCast"
-            className="form-control"
-            placeholder="Imię i nazwisko"
-          />
-        </div>
+				<div className="form-group mb-2">
+					<label htmlFor="inputCast" className="form-label">
+						Obsada
+					</label>
+					<input
+						type="text"
+						id="inputCast"
+						className="form-control"
+						placeholder="Imię i nazwisko"
+					/>
+				</div>
 
-        <div className="form-group">
-          <input
-            type="button"
-            className="btn btn-info col-sm-12"
-            value="Szukaj"
-          />
-        </div>
-      </form>
+				<div className="form-group">
+					<input type="button" className="btn btn-info col-sm-12" value="Szukaj" />
+				</div>
+			</form>
 
-      <MovieTable movies={movies} />
+			<MovieTable movies={movies} />
 
-      <h1>Filmy wg gatunku</h1>
+			<h1>Filmy wg gatunku</h1>
+			<MovieListByGenre movies={movies} />
 
-      <h1>Filmy wg obsady</h1>
-    </div>
-  );
+			<h1>Filmy wg obsady</h1>
+		</div>
+	);
 }
 
 export default App;
---
+++ zad-3/src/components/MovieList/MovieListByGenre.tsx
@@ -0,0 +1,41 @@
+import { type Movie } from "@/types";
+import { MovieList } from "@/components/MovieList/MovieList";
+
+type MovieListByGenreProps = {
+	movies: Movie[];
+};
+
+export function MovieListByGenre({ movies }: MovieListByGenreProps) {
+	const moviesByGenre = new Map<string, Movie[]>();
+
+	movies.map((movie) => {
+		movie.genres.forEach((genre) => {
+			if (!moviesByGenre.has(genre)) {
+				moviesByGenre.set(genre, [movie]);
+			}
+
+			const genreMovies = moviesByGenre.get(genre);
+			if (genreMovies) {
+				moviesByGenre.set(genre, [movie, ...genreMovies]);
+			}
+		});
+	});
+
+	const genres = Array.from(moviesByGenre.keys());
+
+	return (
+		<>
+			{genres.map((genre) => {
+				const movies = moviesByGenre.get(genre);
+				if (!movies) return <></>;
+
+				return (
+					<div key={genre}>
+						<h2>{genre}</h2>
+						<MovieList movies={movies} />
+					</div>
+				);
+			})}
+		</>
+	);
+}

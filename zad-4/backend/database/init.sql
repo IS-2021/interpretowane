987e1a695f132041d573aadfbc8718d31f560e98
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-29 21:59:50 +0100

    feat(zad4): Add even more initial products to init script

--- zad-4/backend/database/init.sql
+++ zad-4/backend/database/init.sql
@@ -59,8 +59,19 @@ INSERT INTO OrderStatuses (orderStatusId, status) VALUES
 INSERT INTO Categories (name) VALUES ('Owoce'), ('Warzywa');
 INSERT INTO Products (name, description, unitPrice, unitWeight, categoryId) 
 VALUES
-    ('Jabłka', 'Świeże i smaczne, prosto z ogrodu sąsiada', 10000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
-    ('Pomarańcze', 'Bloody oranges!', 16000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
-    ('Wiśnie', 'To te z pestkami. Chyba.', 35000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
-    ('Kalafior', 'Bułka tarta sprzedawana osobno.', 15000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
-    ('Pomidor', 'Dyskusyjna kategoria, ale bezdyskusyjnie smaczny.', 22000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Warzywa'));
+    ('Jabłka', 'Świeże i smaczne, prosto z ogrodu sąsiada', 1000, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Pomarańcze', 'Bloody oranges!', 1600, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Wiśnie', 'To te z pestkami. Chyba.', 3500, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Kalafior', 'Bułka tarta sprzedawana osobno.', 1500, 1000, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Pomidor', 'Dyskusyjna kategoria, ale bezdyskusyjnie smaczny.', 2200, 1000, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Gruszki', 'Słodkie gruszki z sadu', 1200, 1000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Mandarynki', 'Mini pomarańcze pełne witaminy', 1800, 800, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Maliny', 'Świeże maliny idealne do deserów', 4200, 500, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Brokuły', 'Zielone brokuły, bogate w błonnik', 2000, 800, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Marchewki', 'Słodkie marchewki prosto z pola', 900, 800, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Cukinia', 'Cukinia gotowa do grillowania', 2500, 700, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Arbuzy', '80% wody, 20% pestek', 3000, 5000, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Brzoskwinie', 'Dojrzałe brzoskwinie z własnego sadu', 1800, 1200, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Jeżyny', 'Dzikie jeżyny, pełne witamin', 4500, 800, (SELECT categoryId FROM Categories WHERE name = 'Owoce')),
+    ('Szparagi', 'Świeże szparagi, idealne do sałatek', 2800, 500, (SELECT categoryId FROM Categories WHERE name = 'Warzywa')),
+    ('Ziemniaki', 'Firmowe ziemniaki, doskonałe do gotowania', 1200, 2000, (SELECT categoryId FROM Categories WHERE name = 'Warzywa'));

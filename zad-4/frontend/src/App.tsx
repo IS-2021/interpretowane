df51619717ea3879182aee112b528d5014d8fe83
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-29 21:49:21 +0100

    refactor(zad4): Move redux provider to main.tsx

--- zad-4/frontend/src/App.tsx
+++ zad-4/frontend/src/App.tsx
@@ -1,10 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import React from "react";
-import { Provider as ReduxProvider } from "react-redux";
 import { HomePage } from "@/routes/page";
 import { Error } from "@/routes/error";
 import { Toaster } from "@/components/UI/Toaster";
-import { store } from "@/store/store";
 
 const router = createBrowserRouter([
 	{
@@ -16,13 +14,11 @@ const router = createBrowserRouter([
 
 export function App() {
 	return (
-		<ReduxProvider store={store}>
-			<div className="mx-auto flex h-0 min-h-screen flex-col">
-				<div className="mx-5 my-6 flex-grow">
-					<RouterProvider router={router} />
-				</div>
-				<Toaster />
+		<div className="mx-auto flex h-0 min-h-screen flex-col">
+			<div className="mx-5 my-6 flex-grow">
+				<RouterProvider router={router} />
 			</div>
-		</ReduxProvider>
+			<Toaster />
+		</div>
 	);
 }
--- zad-4/frontend/src/main.tsx
+++ zad-4/frontend/src/main.tsx
@@ -1,12 +1,16 @@
 import "./globals.css";
 import "@fontsource-variable/inter";
-import ReactDOM from "react-dom/client";
 import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import { App } from "@/App";
+import { store } from "@/store/store";
 
 // eslint-disable-next-line import/no-named-as-default-member
 ReactDOM.createRoot(document.querySelector("#root")!).render(
 	<React.StrictMode>
-		<App />
+		<ReduxProvider store={store}>
+			<App />
+		</ReduxProvider>
 	</React.StrictMode>,
 );

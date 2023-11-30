4e0ed261c711162397193e9772e60f6d0148dc0e
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-11-30 19:09:18 +0100

    feat(zad4): Add CORS for management port

--- zad-4/backend/src/index.ts
+++ zad-4/backend/src/index.ts
@@ -1,14 +1,46 @@
+import * as console from "console";
 import Fastify from "fastify";
-import cors from "@fastify/cors";
+import cors, { type FastifyCorsOptionsDelegateCallback } from "@fastify/cors";
 import { apiRouter } from "@/api/apiRouter";
 import { getLoggerFromEnv } from "@/logger";
+
+const ADMIN_PORT = "5500";
+const publicURLs = {
+	GET: ["/products", "/categories"] as const,
+	POST: ["/orders"] as const,
+	OPTIONS: ["/orders"] as const,
+};
 
 const fastify = Fastify({
 	logger: getLoggerFromEnv(),
 });
 
-await fastify.register(cors, {
-	origin: true,
+await fastify.register(cors, (): FastifyCorsOptionsDelegateCallback => {
+	return (req, callback) => {
+		const corsOptions = {
+			origin: false,
+			credentials: true,
+		};
+
+		const origin = new URL(req.headers.origin as string);
+		if (origin.port === ADMIN_PORT) {
+			console.log("Admin access detected");
+			console.log(req.method, req.url);
+			corsOptions.origin = true;
+		} else if (req.method in publicURLs) {
+			const routes = publicURLs[req.method];
+			if (!routes) {
+				callback(new Error("Invalid method"));
+				return;
+			}
+
+			if (routes.includes(req.url)) {
+				corsOptions.origin = true;
+			}
+		}
+
+		callback(null, corsOptions);
+	};
 });
 await fastify.register(apiRouter);
 

ae21668f64aef62b7c5cdda2860ac328780cb325
jkazimierczak <77862767+jkazimierczak@users.noreply.github.com>
2023-12-03 02:10:37 +0100

    refactor(zad4): Add EditProductSheet.tsx

--- zad-4/frontend/src/routes/admin/page.tsx
+++ zad-4/frontend/src/routes/admin/page.tsx
@@ -1,5 +1,3 @@
-import { AlertTriangleIcon, PencilIcon } from "lucide-react";
-import { useState } from "react";
 import {
 	Table,
 	TableBody,
@@ -9,69 +7,28 @@ import {
 	TableHeader,
 	TableRow,
 } from "@/components/UI/Table";
-import { updateProduct, useGetProducts } from "@/api/products";
-import { Button } from "@/components/UI/Button";
+import { useGetProducts } from "@/api/products";
 import { formatPrice } from "@/lib/utils";
-import { type Product } from "@/api/types";
 import { Label } from "@/components/UI/Label";
 import { Input } from "@/components/UI/Input";
 import { useGetCategories } from "@/api/categories";
 import { CategorySelect } from "@/components/CategorySelect";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/UI/Tabs";
-import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/components/UI/Sheet";
-import { Alert, AlertDescription, AlertTitle } from "@/components/UI/Alert";
-import { ApiError } from "@/lib/fetcher";
-import { toast } from "@/components/UI/useToast";
 import { useFilteredProducts } from "@/hooks/useFilteredProducts";
+import { EditProductSheet } from "@/components/admin/EditProductSheet";
 
 export function AdminPage() {
-	const { data: products, isLoading: areProductsLoading, mutate } = useGetProducts();
+	const {
+		data: products,
+		isLoading: areProductsLoading,
+		mutate: mutateProducts,
+	} = useGetProducts();
 	const { data: categories, isLoading: areCategoriesLoading } = useGetCategories();
 	const { filteredProducts, handleSearchName, handleSearchCategory } = useFilteredProducts(
 		products ?? [],
 	);
 
-	const [productCategory, setProductCategory] = useState("");
-	const [errorMessage, setErrorMessage] = useState("");
-
 	if (areProductsLoading || !products || areCategoriesLoading || !categories) return null;
-
-	const handleProductEdit = async (event: React.FormEvent<HTMLFormElement>, product: Product) => {
-		event.preventDefault();
-
-		type ProductUpdateData = Omit<Product, "categoryname"> & Partial<Pick<Product, "categoryname">>;
-		const productUpdateData: ProductUpdateData = { ...product };
-		delete productUpdateData["categoryname"];
-
-		const formData = new FormData(event.target as HTMLFormElement);
-
-		if (formData.get("unitprice")) {
-			productUpdateData.unitprice = formData.get("unitprice") as string;
-		}
-		if (formData.get("unitweight")) {
-			productUpdateData.unitweight = formData.get("unitweight") as string;
-		}
-		const categoryId = categories[productCategory];
-		if (categoryId) {
-			productUpdateData.categoryid = categoryId;
-			setProductCategory("");
-		}
-
-		try {
-			const res = await updateProduct(productUpdateData);
-			if (res.ok) {
-				await mutate();
-				setErrorMessage("");
-				toast({ description: "Zapisano zmiany", variant: "success" });
-			}
-		} catch (error) {
-			if (error instanceof ApiError) {
-				console.warn(error);
-				const data = (await error.response.json()) as { message: string };
-				setErrorMessage(data.message);
-			}
-		}
-	};
 
 	return (
 		<div className="mx-auto w-full max-w-screen-md">
@@ -117,56 +74,7 @@ export function AdminPage() {
 										<TableCell>{product.description}</TableCell>
 										<TableCell>{formatPrice(parseInt(product.unitprice))}</TableCell>
 										<TableCell>
-											<Sheet>
-												<SheetTrigger asChild>
-													<Button>
-														Edytuj
-														<PencilIcon className="ml-2" size={16} />
-													</Button>
-												</SheetTrigger>
-												<SheetContent>
-													<SheetHeader>
-														<SheetTitle>Edytuj {product.name}</SheetTitle>
-													</SheetHeader>
-													<div>
-														{errorMessage && (
-															<Alert variant="destructive">
-																<AlertTriangleIcon className="h-4 w-4" />
-																<AlertTitle>Wystąpił błąd</AlertTitle>
-																<AlertDescription>{errorMessage}</AlertDescription>
-															</Alert>
-														)}
-														<form onSubmit={(event) => handleProductEdit(event, product)}>
-															<div className="mb-4">
-																<Label className="mb-4">Cena jednostkowa [grosz]</Label>
-																<Input
-																	type="number"
-																	name="unitprice"
-																	defaultValue={product.unitprice}
-																></Input>
-															</div>
-															<div className="mb-4">
-																<Label className="mb-2">Waga [gram]</Label>
-																<Input
-																	type="number"
-																	name="unitweight"
-																	defaultValue={product.unitweight}
-																></Input>
-															</div>
-															<div className="mb-4">
-																<Label className="mb-4">Kategoria</Label>
-																<CategorySelect
-																	defaultValue={product.categoryname.toLowerCase()}
-																	onValueChange={(val) => setProductCategory(val)}
-																	categories={Object.keys(categories)}
-																	showAll={false}
-																/>
-															</div>
-															<Button className="w-full">Zapisz</Button>
-														</form>
-													</div>
-												</SheetContent>
-											</Sheet>
+											<EditProductSheet product={product} mutateProducts={mutateProducts} />
 										</TableCell>
 									</TableRow>
 								))}
